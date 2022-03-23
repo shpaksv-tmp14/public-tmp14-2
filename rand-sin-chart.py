23a84860bf70f2558ce06e9e544628f0220db61c
shpaksv-tmp14 <98563943+shpaksv-tmp14@users.noreply.github.com>
2022-03-23 10:11:54 -0400

    Create rand-sin-chart.py

---
+++ rand-sin-chart.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+t = np.arange(0.00, 10*np.pi, 0.05)
+data = pd.DataFrame({
+  "rand": np.random.rand(1, len(t))[0] + 5,
+  "sin": np.random.rand(1, len(t))[0] + 3*sin(t) - 2*sin(0.5*t) + sin(0.25*t)
+})
+st.chart_line(data)
