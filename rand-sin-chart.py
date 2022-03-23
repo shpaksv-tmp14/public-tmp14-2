9b8a8addcb24bd1d26339a27fe94bb9b254653f8
shpaksv-tmp14 <98563943+shpaksv-tmp14@users.noreply.github.com>
2022-03-23 10:14:35 -0400

    Update rand-sin-chart.py

--- rand-sin-chart.py
+++ rand-sin-chart.py
@@ -5,6 +5,6 @@ import numpy as np
 t = np.arange(0.00, 10*np.pi, 0.05)
 data = pd.DataFrame({
   "rand": np.random.rand(1, len(t))[0] + 5,
-  "sin": np.random.rand(1, len(t))[0] + 3*sin(t) - 2*sin(0.5*t) + sin(0.25*t)
+  "sin": np.random.rand(1, len(t))[0] + 3*np.sin(t) - 2*np.sin(0.5*t) + np.sin(0.25*t)
 })
 st.chart_line(data)
